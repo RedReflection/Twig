164ddc1985da3fd3864dd91b5f545ae801d7962f
RedReflection <daniel.adams1@btinternet.com>
2020-03-23 19:38:30 +0000

    changed iteration i calculation

--- Twig/groupArrayElements.py
+++ Twig/groupArrayElements.py
@@ -1,38 +1,47 @@
 import numpy as np
 
-def groupArrayElements(arr, N):
+def groupArrayElements(array, N):
+    # Check that N is a positive non-zero integer
     if type(N) != int:
-        raise NotImplementedError( "N {} needs to be an integer".format(N))
-    if N < 0 :
-        raise NotImplementedError( "N {} needs to be a positive integer".format(N))
+        raise NotImplementedError("N {} needs to be an integer".format(N))
+    if N < 0:
+        raise NotImplementedError("N {} needs to be a positive integer".format(N))
     if N == 0:
         raise ZeroDivisionError("N equals 0")
-    arrLen = len(arr)
-    if N < arrLen:
-        if arr and N:
+
+    arrayLength = len(array)
+    if N < arrayLength:
+        if array and N:
             res = []
-            fl = int(np.floor(arrLen/N))
-            cl = int(np.ceil(arrLen/N))
-            if cl * (N-1) >= arrLen:
-                arrSize = fl
+            # Round up (ceiling) and round down (floor) division of array length by N
+            # Check whether N-1 subarrays can be made with upper rounding to fill array
+            floor = int(np.floor(arrayLength/N))
+            ceiling = int(np.ceil(arrayLength/N))
+            # If N-1 subarrays can be created using the ceiling for the size of each subarray, use ceiling
+            # Using N-1 to leave one array for the remainder of the division
+            if ceiling * (N-1) < arrayLength:
+                subarrayLength = ceiling
+            # If N-1 subarrays can't be created using the ceiling for the size of each subarray, use floor
             else:
-                arrSize = cl
+                subarrayLength = floor
 
-            for i in range(0, N-1):
-                i = i*arrSize
-                arrI = arr[i:i+arrSize]
-                res.append(arrI)
-                arrI = []
+            # Create all subarrays except the last one
+            for i in range(0, (N-1)*subarrayLength, subarrayLength):
+                subarray = array[i:i + subarrayLength]
+                res.append(subarray)
+                subarray = []
 
-            i = i+arrSize
-            arrI = arr[i::]
-            if arrI:
-                res.append(arrI)
+            # Create the last subarray with the remaining elements
+            i = (N-1)*subarrayLength
+            subarray = array[i::]
+            if subarray:
+                res.append(subarray)
 
             return(res)
         return []
     else:
-        if arrLen == 0:
+        # Exceptions if array is empty and if N is greater than length of array
+        if arrayLength == 0:
             raise Exception("Array is empty and cannot be divided by N: {}".format(N))
         else:
-            raise Exception("N: {} is greater than length of array: {}".format(N, arrLen))
+            raise Exception("N: {} is greater than length of array: {}".format(N, arrayLength))
